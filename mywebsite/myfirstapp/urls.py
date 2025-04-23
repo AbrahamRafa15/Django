ca9fe630b286cf3adfa6bb623e75c9335449dca1
AbrahamMartinez15 <abraham.martinez1503@gmail.com>
2025-04-22 18:00:24 -0600

    Update the app

--- mywebsite/myfirstapp/urls.py
+++ mywebsite/myfirstapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:estudiante_id>/detalles/", views.detalles, name="detalles"),
+    path("<int:estudiante_id>/carreras/", views.carreras, name="carreras"),
+    path("<int:estudiante_id>/agrega_carrera/", views.agrega_carrera, name="agrega_carrera"),
 ]
--- mywebsite/myfirstapp/views.py
+++ mywebsite/myfirstapp/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Estudiante
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola a todos desde myfirstapp")
+    estudiantes = Estudiante.objects.order_by("nombre")
+    context = {
+        "estudiantes": estudiantes,
+    }
+    return render(request, "myfirstapp/index.html", context)
+
+def detalles(request, estudiante_id):
+    estudiante = get_object_or_404(Estudiante, pk=estudiante_id) 
+    return render(request, "myfirstapp/detalles.html", {"estudiante": estudiante})
+
+def carreras(request, estudiante_id):
+    return HttpResponse("Carreras del estudiante %s." %estudiante_id)
+
+def agrega_carrera(request, estudiante_id):
+    return HttpResponse("Agregando una carrera a %s." %estudiante_id)
